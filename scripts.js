f6a4df235939bbdfab1ba7e42020e3d7af412bc6
therenow9 <jerm.s222@gmail.com>
2022-02-15 13:09:40 -0500

    added comments, changed navbar to fixed top to fix glitching

--- scripts.js
+++ scripts.js
@@ -2,3 +2,4 @@ $(".navbar-collapse a").click(function () {
   $(".navbar-collapse").collapse("hide");
 });
 // auto close navbar
+// not sure if this works is doing anything but i am scared to remove it
