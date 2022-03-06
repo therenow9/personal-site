81c13111f64500468e1a78c007b23d93d253e84a
Jeremy Scheuerman <jerm.s222@gmail.com>
2022-03-06 18:39:21 -0500

    changed full stack project section

--- scripts.js
+++ scripts.js
@@ -2,4 +2,3 @@ $(".navbar-collapse a").click(function () {
   $(".navbar-collapse").collapse("hide");
 });
 // auto close navbar
-// not sure if this works is doing anything but i am scared to remove it
