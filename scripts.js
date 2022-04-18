128bca80cad15675909d966178eede0db1cedea6
Jeremy Scheuerman <jerm.s222@gmail.com>
2022-04-18 17:42:10 -0400

    copmletely removed bootsrap, implemented pure javascript navbar, shortened some sections a bit more

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-$(".navbar-collapse a").click(function () {
-  $(".navbar-collapse").collapse("hide");
-});
-// auto close navbar
+const hamburger = document.querySelector(".hamburger ");
+const nav_menu = document.querySelector(".nav__link");
+const nav_item = document.querySelectorAll(".nav__link");
+hamburger.addEventListener("click", toggleMenu);
+nav_item.forEach((n) => n.addEventListener("click", toggleMenu));
+
+function toggleMenu() {
+  nav_menu.classList.toggle("hide");
+}
